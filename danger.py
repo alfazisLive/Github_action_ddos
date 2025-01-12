affa51d05512b2b67ebf3bebb3f260e1c0814f63
alfazisLive <alfazkhan62563@gmail.com>
2025-01-12 23:27:55 +0530

    Update danger.py

--- danger.py
+++ danger.py
@@ -2,8 +2,8 @@ import asyncio
 from telegram import Update
 from telegram.ext import Application, CommandHandler, CallbackContext
 
-TELEGRAM_BOT_TOKEN = 'BOT_TOKEN'
-ADMIN_USER_ID = 123456789
+TELEGRAM_BOT_TOKEN = '8176603194:AAH0yJvKJ1kb1iTBJCCKkRQhohhXYHhhUJg'
+ADMIN_USER_ID = 6336630729
 USERS_FILE = 'users.txt'
 attack_in_progress = False
 
